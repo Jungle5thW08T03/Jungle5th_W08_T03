c4e6e0c397c4d7ad34db2986308c48c786390c41
woo_j <woo_j@ubuntu18.04.myguest.virtualbox.org>
2024-05-23 20:11:35 +0900

    feat: argument passing

--- include/threads/interrupt.h
+++ include/threads/interrupt.h
@@ -1,22 +1,21 @@
-/* 인터럽트 활성화, 비활성화 관련 자료형&함수 */
-
 #ifndef THREADS_INTERRUPT_H
 #define THREADS_INTERRUPT_H
 
 #include <stdbool.h>
 #include <stdint.h>
 
-/* Interrupts on or off */
+/* Interrupts on or off? */
+// Intr_level이 가질 수 있는 값 = INTR_OFF, INTR_ON
 enum intr_level
 {
-	INTR_OFF, /* Interrupts disabled. */
-	INTR_ON	  /* Interrupts enabled. */
+	INTR_OFF, /* 인터럽트 비활성화 상태 Interrupts disabled. */
+	INTR_ON	  /* 인터럽트 활성화 상태 Interrupts enabled. */
 };
 
-enum intr_level intr_get_level(void);			 /* 현재 인터럽트의 상태 리턴 */
-enum intr_level intr_set_level(enum intr_level); /* 현재 상태에 따라 인터럽트를 활성화하거나 비활성화하는 함수 - 인터럽트의 이전 상태 리턴 */
-enum intr_level intr_enable(void);				 /* 인터럽트를 활성화하는 함수 - 인터럽트의 이전 상태 리턴 */
-enum intr_level intr_disable(void);				 /* 인터럽트를 비활성화하는 함수 - 인터럽트의 이전 상태 리턴 */
+enum intr_level intr_get_level(void);			 // 현재 인터럽트 상태 반환
+enum intr_level intr_set_level(enum intr_level); // 현재 상태에 따라 인터럽트를 활성화/비활성화 하는 함수, 인터럽트의 이전 상태 반환
+enum intr_level intr_enable(void);				 // 인터럽트 활성화, 인터럽트의 이전 상태 반환
+enum intr_level intr_disable(void);				 // 인터럽트 비활성화, 인터럽트의 이전 상태 반환
 
 /* Interrupt stack frame. */
 struct gp_registers
@@ -29,24 +28,24 @@ struct gp_registers
 	uint64_t r10;
 	uint64_t r9;
 	uint64_t r8;
-	uint64_t rsi;
-	uint64_t rdi;
-	uint64_t rbp;
-	uint64_t rdx;
-	uint64_t rcx;
-	uint64_t rbx;
-	uint64_t rax;
+	uint64_t rsi; /* 인덱스 레지스터 */
+	uint64_t rdi; /* 인덱스 레지스터 */
+	uint64_t rbp; /* 스택 메모리 시작 주소 */
+	uint64_t rdx; /* 범용 레지스터 */
+	uint64_t rcx; /* 범용 레지스터 */
+	uint64_t rbx; /* 범용 레지스터 */
+	uint64_t rax; /* 범용 레지스터 */
 } __attribute__((packed));
 
 struct intr_frame
 {
 	/* Pushed by intr_entry in intr-stubs.S.
 	   These are the interrupted task's saved registers. */
-	struct gp_registers R;
+	struct gp_registers R; /* 범용 레지스터 = 120byte */
 	uint16_t es;
 	uint16_t __pad1;
 	uint32_t __pad2;
-	uint16_t ds;
+	uint16_t ds; /* 세그먼트 관리 */
 	uint16_t __pad3;
 	uint32_t __pad4;
 	/* Pushed by intrNN_stub in intr-stubs.S. */
@@ -57,12 +56,12 @@ struct intr_frame
 	uint64_t error_code;
 	/* Pushed by the CPU.
 	   These are the interrupted task's saved registers. */
-	uintptr_t rip;
-	uint16_t cs;
+	uintptr_t rip; /* Program Counter */
+	uint16_t cs;   /* 세그먼트 관리 */
 	uint16_t __pad5;
 	uint32_t __pad6;
-	uint64_t eflags;
-	uintptr_t rsp;
+	uint64_t eflags; /* cpu 상태 나타내는 정보 */
+	uintptr_t rsp;	 /* Stack Pointer */
 	uint16_t ss;
 	uint16_t __pad7;
 	uint32_t __pad8;
